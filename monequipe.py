9de926d60313ca5d782cf6809eb91bc993171586
MarièneWan <mariene.wan@laposte.fr>
2015-02-09 11:40:59 +0100

    teams

--- monequipe.py
+++ monequipe.py
@@ -5,14 +5,23 @@ from strats import RandomStrategy
 
 team1=SoccerTeam("team1")
 team1.add_player(SoccerPlayer("t1j1",FonceurStrategy()))
-team1.add_player(SoccerPlayer("t1j2",PStrategy()))
 
 
+team2=SoccerTeam("team2")
+team2.add_player(SoccerPlayer("t2j1",FonceurStrategy()))
+team2.add_player(SoccerPlayer("t2j2",PStrategy()))
+
+team3=SoccerTeam("team3")
+team3.add_player(SoccerPlayer("t3j1",FonceurStrategy()))
+team3.add_player(SoccerPlayer("t3j2",PStrategy()))
+team3.add_player(SoccerPlayer("t3j3",FonceurStrategy()))
+team3.add_player(SoccerPlayer("t3j4",PStrategy()))
 
 
-if __name__== "__main__":
-	battle=SoccerBattle(team1,team1.copy())
-	obs=PygletObserver()
-	obs.set_soccer_battle(battle)
-	pyglet.app.run()
+teams = [team1, team2, team3]
+#if __name__== "__main__":
+#	battle=SoccerBattle(team1,team1.copy())
+	#obs=PygletObserver()
+	#obs.set_soccer_battle(battle)
+	#pyglet.app.run()
 
