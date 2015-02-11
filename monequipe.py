d1080b8f324509e1942c1064e36c689cf4464d1d
MarièneWan <mariene.wan@laposte.fr>
2015-02-11 14:49:50 +0100

    maj

--- monequipe.py
+++ monequipe.py
@@ -4,21 +4,21 @@ from soccersimulator import pyglet
 from outils import ComposeStrategy,FonceurStrategy,Defenseur
 #from outils import 
 
-team1=SoccerTeam("team1")
+team1=SoccerTeam("Poireau")
 team1.add_player(SoccerPlayer("t1j1",FonceurStrategy()))
 
 
-team2=SoccerTeam("team2")
+team2=SoccerTeam("Patate")
 team2.add_player(SoccerPlayer("t2j1",FonceurStrategy()))
 team2.add_player(SoccerPlayer("t2j2",FonceurStrategy()))
 
-team3=SoccerTeam("team3")
+team3=SoccerTeam("Aubergine")
 team3.add_player(SoccerPlayer("t3j1",FonceurStrategy()))
 team3.add_player(SoccerPlayer("t3j2",Defenseur()))
 team3.add_player(SoccerPlayer("t3j3",Defenseur()))
 team3.add_player(SoccerPlayer("t3j4",FonceurStrategy()))
 
-team4=SoccerTeam("team4")
+team4=SoccerTeam("Carotte")
 team4.add_player(SoccerPlayer("t4j2",FonceurStrategy()))
 team4.add_player(SoccerPlayer("t4j1",FonceurStrategy()))
 team4.add_player(SoccerPlayer("t4j3",Defenseur()))
