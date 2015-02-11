0a5237aa611fe1947c40a26fa0792e74abfc0eec
MarièneWan <mariene.wan@laposte.fr>
2015-02-11 19:23:32 +0100

    maj

--- monequipe.py
+++ monequipe.py
@@ -34,7 +34,7 @@ team5.add_player(SoccerPlayer("t5j1",Mix()))
 team5.add_player(SoccerPlayer("t5j3",Defenseur()))
 team5.add_player(SoccerPlayer("t5j4",FonceurStrategy()))
 
-teams = [team1, team2, team3, team4,team5]
+teams = [team1, team2, team3, team4,team5,team6]
 #if __name__== "__main__":
 #	battle=SoccerBattle(team1,team1.copy())
 	#obs=PygletObserver()
