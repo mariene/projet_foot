659fdf5096aab2cf5e88e09789ccf343ef4b5ae3
mariene <mariene.wan@laposte.net>
2015-02-13 00:14:40 +0100

    maj

--- monequipe.py
+++ monequipe.py
@@ -2,7 +2,6 @@ from soccersimulator import SoccerBattle, SoccerPlayer, SoccerTeam
 from soccersimulator import PygletObserver,ConsoleListener,LogListener
 from soccersimulator import pyglet
 from outils import *
-#from outils import 
 
 team1=SoccerTeam("Poireaux")
 team1.add_player(SoccerPlayer("t1j1",FonceurStrategy()))
@@ -12,9 +11,9 @@ team2=SoccerTeam("Patates")
 team2.add_player(SoccerPlayer("t2j1",FonceurStrategy()))
 team2.add_player(SoccerPlayer("t2j2",Defenseur()))
 
-team6=SoccerTeam("Artichauts")
-team6.add_player(SoccerPlayer("t6j1",Mix()))
-team6.add_player(SoccerPlayer("t6j2",FonceurStrategy()))
+#team6=SoccerTeam("Artichauts")
+#team6.add_player(SoccerPlayer("t6j1",Mix()))
+#team6.add_player(SoccerPlayer("t6j2",FonceurStrategy()))
 
 team3=SoccerTeam("Aubergines")
 team3.add_player(SoccerPlayer("t3j1",FonceurStrategy()))
@@ -34,7 +33,7 @@ team5.add_player(SoccerPlayer("t5j1",Mix()))
 team5.add_player(SoccerPlayer("t5j3",Defenseur()))
 team5.add_player(SoccerPlayer("t5j4",FonceurStrategy()))
 
-teams = [team1, team2, team3, team4,team5,team6]
+teams = [team1, team2, team3, team4,team5]
 #if __name__== "__main__":
 #	battle=SoccerBattle(team1,team1.copy())
 	#obs=PygletObserver()
